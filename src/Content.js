a8020d1a13808de4836f3cef629902be142b9c6c
shubhharkawat <shubhharkawat1406@gmail.com>
2021-12-06 13:52:54 +0530

    First Commit

---
+++ src/Content.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Content.css'
+import Infocard from './Infocard'
+function Content() {
+    return (
+        <div className="Content">
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <Infocard />
+            <div>  </div>
+        </div>
+    )
+}
+
+export default Content
